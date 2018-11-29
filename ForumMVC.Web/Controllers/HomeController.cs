482377e27eb4a36eaab40f3d84449bf9ce46b3bb
simakovich@gmail.com <simakovich@gmail.com>
2018-11-29 16:42:38 +0300

    Изменил 29112018

--- ForumMVC.Web/Controllers/HomeController.cs
+++ ForumMVC.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace ForumMVC.Web.Controllers
-{
+{//Comment
     public class HomeController : Controller
     {
         public ActionResult Index()
